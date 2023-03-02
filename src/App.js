79ccc9ae8c6f1c4e1809d82d7f9be0a7bc5764e8
LeeTaeKyung <leek9708@gmail.com>
2023-03-02 13:21:19 +0900

    import

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Nav, Navbar, Container } from "react-bootstrap";
+import bg from "./img/bg.png";
+import { useState } from "react";
+import { data } from "./data"; // 확장자 생략해도 봐줌
 
 function App() {
+  const [shoes] = useState(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">태키 쇼핑몰</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div
+        className="main-bg"
+        style={{ backgroundImage: "url(" + bg + ")" }}
+      ></div>
+      <div className="container">
+        <div className="row">
+          {shoes.map((item) => {
+            return <ShoesList item={item} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
+
+const ShoesList = ({ item }) => {
+  return (
+    <div className="col-md-4">
+      <img src={item.url} width="80%" />
+      <h4>{item.title}</h4>
+      <p>{item.content}</p>
+    </div>
+  );
+};
 
 export default App;
